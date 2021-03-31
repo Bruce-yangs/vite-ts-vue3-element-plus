24c6bd276b32282562c9e933384166765c905e78
Bruce yangs <469402333@qq.com>
2021-04-01 00:56:19 +0800

    add GlobalHeader

--- src/App.vue
+++ src/App.vue
@@ -3,39 +3,60 @@
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
   <Index/> -->
   <div id="app" class="container">
-    <router-view />
+    <!-- <router-view /> -->
+    <GlobalHeader :user="currentUser"/>
+    <ColumnList :list="list" />
   </div>
 </template>
 
 <script lang="ts">
-import 'bootstrap/dist/css/bootstrap.min.css'
-import ColumnList,{ColumnProps} from './components/ColumnList.vue'
-// import ColumnList,{ColumnProps} from '@/components/ColumnList.vue'
-import { defineComponent } from 'vue'
+import "bootstrap/dist/css/bootstrap.min.css";
+import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
+import GlobalHeader, { UserProps } from "./components/GlobalHeader.vue";
+import { defineComponent } from "vue";
 // import HelloWorld from './components/HelloWorld.vue'
 // import Index from './views/Index.vue'
-const testData:ColumnProps[] =[
+const currentUser: UserProps = { isLogin: true,name:'viki' };
+const testData: ColumnProps[] = [
   {
-    id:1,
-    title:'test1的专栏',
-    description:'这是test1专栏，有一段非常有意思的见解，可以更新下',
-    avatar:'https://sf3-ttcdn-tos.pstatp.com/img/user-avatar/612ecba34e3035f2f395dff412dcc8b3~300x300.image'
-  },{
-    id:2,
-    title:'test2的专栏',
-    description:'这是test2专栏，有一段非常有意思的见解，可以更新下',
-    avatar:'https://sf3-ttcdn-tos.pstatp.com/img/user-avatar/612ecba34e3035f2f395dff412dcc8b3~300x300.image'
-  }
-]
+    id: 1,
+    title: "test1的专栏",
+    description: "这是test1专栏，有一段非常有意思的见解，可以更新下",
+    avatar:
+      "https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg",
+  },
+  {
+    id: 2,
+    title: "test2的专栏",
+    description: "这是test2专栏，有一段非常有意思的见解，可以更新下",
+    avatar:
+      "https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg",
+  },
+  {
+    id: 3,
+    title: "test3的专栏",
+    description: "这是test3专栏，有一段非常有意思的见解，可以更新下",
+    avatar:
+      "https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg",
+  },
+  {
+    id: 4,
+    title: "test4的专栏",
+    description: "这是test4专栏，有一段非常有意思的见解，可以更新下",
+    // avatar:
+    // "https://sf3-ttcdn-tos.pstatp.com/img/user-avatar/612ecba34e3035f2f395dff412dcc8b3~300x300.image",
+  },
+];
 export default defineComponent({
-  name: 'App',
-  setup(){
-    return {list:testData}
+  name: "App",
+  setup() {
+    return { list: testData ,currentUser};
   },
   components: {
-    ColumnList
-  }
-})
+    ColumnList,
+    GlobalHeader,
+  },
+});
 // export default defineComponent({
 //   name: 'App',
 //   components: {
@@ -45,7 +66,9 @@ export default defineComponent({
 </script>
 
 <style>
-html,body,#app {
+html,
+body,
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -54,6 +77,6 @@ html,body,#app {
   /* margin-top: 60px; */
   width: 100%;
   height: 100%;
-  overflow: hidden;
+  /* overflow: hidden; */
 }
 </style>
---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary justify-content-between mb-4">
+    <a class="navbar-brand  mx-3" href="#">Navbar w/ text</a>
+    <ul v-if="!user.isLogin" class="list-inline mb-0 mx-3">
+      <li class="list-inline-item">
+        <a href="#" class="btn btn-outline-light my-2">登录</a>
+      </li>
+      <li class="list-inline-item">
+        <a href="#" class="btn btn-outline-light my-2">注册</a>
+      </li>
+    </ul>
+    <ul v-else class="list-inline mb-0  mx-3">
+       <li class="list-inline-item">
+        <a href="#" class="btn btn-outline-light my-2">你好 {{user.name}}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, PropType } from "vue";
+export interface UserProps {
+  isLogin: boolean;
+  name?: string;
+  id?: string;
+}
+export default defineComponent({
+  name: "GlobalHeader",
+  props: {
+    user: {
+      type: Object as PropType<UserProps>,
+      required: true,
+    },
+  },
+  setup(props) {
+    // const columnListData = computed(() => {
+    //   return props.list.map((column) => {
+    //     if (!column.avatar) {
+    //       // column.avatar = require('../assets/logo.png')
+    //       column.avatar =
+    //         "https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg";
+    //     }
+    //     return column;
+    //   });
+    // });
+    // // columnList.value = columnListData;
+    // return { props };
+  },
+});
+</script>
+
+<style></style>
